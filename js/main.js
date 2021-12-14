4243fd79a7c910fd9d7d3453f4fb4331b614a71c
canicode3896 <93296722+canicode3896@users.noreply.github.com>
2021-12-14 15:08:47 -0500

    go

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import { console1, console2 } from "./modules/module";
+
+console1();
+console2();
---
+++ js/modules/module.js
@@ -0,0 +1,9 @@
+function console1() {
+    console.log("hello");
+}
+
+function console2() {
+    console.log("bye");
+}
+
+export { console1, console2 };
